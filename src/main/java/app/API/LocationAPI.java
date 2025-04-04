410ace85adc2cc5a7d29ced90b08880c09c452d8
Poseidno228 <danil.t404@gmail.com>
2025-04-04 12:41:07 +0100

    change app server to 0.0.0.0

--- src/main/java/app/API/LocationAPI.java
+++ src/main/java/app/API/LocationAPI.java
@@ -1,22 +1,15 @@
 package app.API;
 
-import app.models.PlaneDb;
-import app.models.Route;
-import app.services.LocationService;
 import app.services.interfaces.IFileService;
-import app.services.interfaces.ILocationService;
 import fi.iki.elonen.NanoHTTPD;
-import org.json.JSONArray;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class LocationAPI extends NanoHTTPD {
     private final IFileService fileService;
 
     public LocationAPI(int port, IFileService fileService) throws IOException {
-        super(port);
+        super("0.0.0.0", port);
         this.fileService = fileService;
         start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
         System.out.println("Server running at: http://localhost: " + port);
